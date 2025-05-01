a4ac1ec3a6c7c33cd708fbe0c0c0c5998f485767
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-05-01 17:45:26 +0200

    chore(crawler): wire infrastructure module

---
+++ src/infrastructure/mod.rs
@@ -0,0 +1,11 @@
+mod crawler_sequential;
+mod fetcher_graphql;
+mod fetcher_retrier;
+mod persister_postgresql;
+mod persister_retrier;
+
+pub use crawler_sequential::*;
+pub use fetcher_graphql::*;
+pub use fetcher_retrier::*;
+pub use persister_postgresql::*;
+pub use persister_retrier::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+mod infrastructure;
 mod interface;
 mod model;
 
+pub use infrastructure::*;
 pub use interface::*;
 pub use model::*;
