7bb22717dc9991bec6c0c816dfea66e08f374610
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2025-05-02 12:23:04 +0200

    refactor(crawler): enhance PostgreSQL persister
    - Use a transaction for persisting data
    - Enhance readbility of the implementation.

--- src/infrastructure/persister_postgresql.rs
+++ src/infrastructure/persister_postgresql.rs
@@ -5,6 +5,20 @@ use sqlx::{PgPool, postgres::PgPoolOptions};
 
 use crate::{Repository, RepositoryPersister, StdResult};
 
+const UPSERT_QUERY: &str = r#"
+WITH upserted AS (
+    INSERT INTO github.repository (repository_name, organization_name, total_stars)
+    VALUES ($1, $2, $3)
+    ON CONFLICT (repository_name, organization_name) DO UPDATE
+    SET total_stars = EXCLUDED.total_stars
+    WHERE github.repository.repository_name IS DISTINCT FROM EXCLUDED.repository_name
+    RETURNING xmax = 0 AS inserted
+)
+SELECT COUNT(*) AS total_inserted
+FROM upserted
+WHERE inserted = true;
+"#;
+
 /// A persister that stores repository metadata in a PostgreSQL database.
 pub struct PostgresSqlPersister {
     pool: PgPool,
@@ -20,6 +34,22 @@ impl PostgresSqlPersister {
                 .await?,
         })
     }
+
+    async fn persist_repository(&self, repository: &Repository) -> StdResult<u32> {
+        let mut transaction = self.pool.begin().await?;
+        let repository_name = &*repository.repository_name().deref();
+        let organization_name = &*repository.organization_name().deref();
+        let repository_stars = *repository.total_stars().deref() as i32;
+        let row: (i64,) = sqlx::query_as(UPSERT_QUERY)
+            .bind(repository_name.to_owned())
+            .bind(organization_name.to_owned())
+            .bind(repository_stars)
+            .fetch_one(&mut *transaction)
+            .await?;
+        transaction.commit().await?;
+
+        Ok(row.0 as u32)
+    }
 }
 
 #[async_trait::async_trait]
@@ -27,35 +57,13 @@ impl RepositoryPersister for PostgresSqlPersister {
     async fn persist(&self, data: &[Repository]) -> StdResult<u32> {
         let mut total_inserted = 0;
         for repository in data {
-            let repository_name = &*repository.repository_name().deref();
-            let organization_name = &*repository.organization_name().deref();
-            let repository_stars = *repository.total_stars().deref() as i32;
-            let row: (i64,) = sqlx::query_as(
-                r#"
-WITH upserted AS (
-    INSERT INTO github.repository (repository_name, organization_name, total_stars)
-    VALUES ($1, $2, $3)
-    ON CONFLICT (repository_name, organization_name) DO UPDATE
-    SET total_stars = EXCLUDED.total_stars
-    WHERE github.repository.repository_name IS DISTINCT FROM EXCLUDED.repository_name
-    RETURNING xmax = 0 AS inserted
-)
-SELECT COUNT(*) AS total_inserted
-FROM upserted
-WHERE inserted = true;
-                "#,
-            )
-            .bind(repository_name.to_owned())
-            .bind(organization_name.to_owned())
-            .bind(repository_stars)
-            .fetch_one(&self.pool)
-            .await?;
-            if row.0 == 0 {
+            let inserted_rows = self.persist_repository(repository).await?;
+            if inserted_rows == 0 {
                 info!("Updated {repository}");
             } else {
                 info!("Inserted {repository}");
             }
-            total_inserted += row.0 as u32;
+            total_inserted += inserted_rows;
         }
 
         Ok(total_inserted)
